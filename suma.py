a4715d9ef175ccf5d77b897f47a9ae31d3dd25b6
JeanSegovia345 <segoviarazojeanpol@gmail.com>
2025-04-25 16:45:41 -0500

    suma de dos numeros

---
+++ suma.py
@@ -0,0 +1,8 @@
+#suma de dos numeros 
+
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+
+resultado = num1 + num2
+
+print("La suma es:", resultado)
