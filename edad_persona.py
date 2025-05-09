7fddfcb3a7842e0881ff2e2399ae5496a5d76e30
JeanSegovia345 <segoviarazojeanpool59@gmail.com>
2025-05-09 08:35:34 +0000

    Calcular edad

---
+++ edad_persona.py
@@ -0,0 +1,9 @@
+# Año actual y año de nacimiento
+anio_actual = 2025
+anio_nacimiento = 2005
+
+# Calcular edad
+edad = anio_actual - anio_nacimiento
+
+# Mostrar resultado
+print("Tu edad es:", edad)
